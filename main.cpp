78b76bfa273ce7bd48251b2ff7335f72094bde4e
Lucas.Stein <ctw02734@criticaltechworks.com>
2023-02-22 17:07:24 +0000

    First commit with initial setup.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+
+    std::cout << "//////////////////////////////////////////////////" << std::endl;
+    std::cout << "/// WELCOME TO THE GAME TIC-TAC-TOE" << std::endl;
+    std::cout << "////////////////////////////////////////" << std::endl << std::endl;
+
+    std::cout << "Enter 1 to start the game OR 0 to exit: ";
+
+    int input;
+    std::cin >> input;
+
+    return 0;
+
+}
